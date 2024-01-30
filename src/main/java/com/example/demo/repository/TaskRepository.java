99a081c40bf4ffb70767be4b2a96bb198c569117
PisarevskiyRE <daimon--@live.ru>
2024-01-30 11:39:30 +0300

    first commit

---
+++ src/main/java/com/example/demo/repository/TaskRepository.java
@@ -0,0 +1,19 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Task;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Transactional
+public interface TaskRepository extends PagingAndSortingRepository<Task, Long>, CrudRepository<Task, Long> {
+
+    @Modifying
+    @Query("update task set done = true where id = :id")
+    void markAsDone(@Param("id") Long id);
+}
